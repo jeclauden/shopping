08a65daf1c92c68bca72ba11e31a2e3c3a135d09
Jean-Claude Ndagijimana <jeclauded@gmail.com>
2022-12-30 23:14:39 +1030

    Add more details to the product details screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -19,8 +19,113 @@ class ProductDetailScreen extends StatelessWidget {
           false, // It's not necessary to rebuild the detail screen if products change. No need to listern
     ).findProductById(productId);
 
+    double imageHeight = MediaQuery.of(context).size.width * 0.8;
+
     return Scaffold(
-      appBar: AppBar(title: Text(product.title)),
+      appBar: AppBar(title: const Text("Item")),
+      body: ListView(
+        children: [
+          Container(
+            decoration:
+                const BoxDecoration(color: Color.fromARGB(26, 105, 100, 100)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Image.network(
+                    product.imageUrl,
+                    height: imageHeight,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 8.0),
+                      padding: const EdgeInsets.all(15),
+                      width: double.infinity,
+                      decoration: const BoxDecoration(color: Colors.white),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.title,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text("Kamenge, Bujumbura"),
+                          const SizedBox(height: 10),
+                          Text(
+                            'AU \$${product.price.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Seller Profile section
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 8.0),
+                      padding: const EdgeInsets.all(20.0),
+                      width: double.infinity,
+                      decoration: const BoxDecoration(color: Colors.white),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(70),
+                            child: Image.network(
+                              'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+                              height: 70,
+                              width: 70,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Claude Ndaj",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "Highly Rated (34 Listings)",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+
+                    // Product description section
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 8.0),
+                      padding: const EdgeInsets.all(15),
+                      width: double.infinity,
+                      decoration: const BoxDecoration(color: Colors.white),
+                      child: Text(
+                        product.description,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
